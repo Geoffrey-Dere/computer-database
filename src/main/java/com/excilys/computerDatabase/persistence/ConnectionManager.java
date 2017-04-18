cb152952131d91f0a8323f2edebc4500c968237a
Geoffrey <excilys@localhost.localdomain>
2017-04-18 11:52:30 +0200

    [refractor] ConnectionManager

--- src/main/java/com/excilys/computerDatabase/persistence/ConnectionManager.java
+++ src/main/java/com/excilys/computerDatabase/persistence/ConnectionManager.java
@@ -11,21 +11,28 @@ public enum ConnectionManager {
 
 	INSTANCE;
 
-	private static final String PATH_FILE = "resources/config/bdd.properties";
+	private static final String PATH_FILE = "config/bdd.properties";
+	private static final String DRIVER = "driver";
+	private static final String BASE = "base";
+	private static final String USER = "user";
+	private static final String PASSWORD = "password";
 
-	private static String url;
+	private static String driver;
+	private static String base;
 	private static String user;
 	private static String password;
 
 	private Connection connection = null;
 
 	static {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		Properties prop = new Properties();
 		try {
-			prop.load(new FileInputStream(PATH_FILE));
-			url = prop.getProperty("url");
-			user = prop.getProperty("user");
-			password = prop.getProperty("password");
+			prop.load(classLoader.getResourceAsStream(PATH_FILE));
+			driver = prop.getProperty(DRIVER);
+			base = prop.getProperty(BASE);
+			user = prop.getProperty(USER);
+			password = prop.getProperty(PASSWORD);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -39,8 +46,8 @@ public enum ConnectionManager {
 
 		try {
 			if (connection == null || connection.isClosed()) {
-				Class.forName("com.mysql.jdbc.Driver");
-				connection = DriverManager.getConnection(url, user, password);
+				Class.forName(driver);
+				connection = DriverManager.getConnection(base, user, password);
 			}
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
