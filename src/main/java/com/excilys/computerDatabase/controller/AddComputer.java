864b041561e7267e3a91a4df01ed91c3efcea141
geoffrey deremetz <geoffrey.deremetz1@etu.univ-lorraine.fr>
2017-04-19 15:01:38 +0200

    [FEATURE] interface client

---
+++ src/main/java/com/excilys/computerDatabase/controller/AddComputer.java
@@ -0,0 +1,21 @@
+package com.excilys.computerDatabase.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddComputer extends HttpServlet {
+
+    /**
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/view/addComputer.jsp").forward(req, resp);
+    }
+
+}
---
+++ src/main/java/com/excilys/computerDatabase/controller/Dashboard.java
@@ -0,0 +1,30 @@
+package com.excilys.computerDatabase.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.computerDatabase.service.ComputerServiceImpl;
+
+public class Dashboard extends HttpServlet {
+
+    /**
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        ComputerServiceImpl computerService = new ComputerServiceImpl();
+        computerService.getAllComputers();
+        
+        
+        
+        
+        this.getServletContext().getRequestDispatcher("/WEB-INF/view/dashboard.jsp").forward(req, resp);
+    }
+    
+}
