a66a00f0aff95afff3b71bab39806337aa80eeff
geoffrey deremetz <geoffrey.deremetz1@etu.univ-lorraine.fr>
2017-04-24 13:56:40 +0200

    [FEAT] hikari

--- src/main/java/com/excilys/computerDatabase/persistence/ConnectionManager.java
+++ src/main/java/com/excilys/computerDatabase/persistence/ConnectionManager.java
@@ -1,13 +1,16 @@
 package com.excilys.computerDatabase.persistence;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public enum ConnectionManager {
 
@@ -15,28 +18,20 @@ public enum ConnectionManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionManager.class);
 
-    private static final String PATH_FILE = "config/bdd.properties";
-    private static final String DRIVER = "driver";
-    private static final String BASE = "base";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
+    private static HikariDataSource hikari;
+    private static HikariConfig config;
 
-    private static String driver;
-    private static String base;
-    private static String user;
-    private static String password;
+    private static final String PATH_FILE = "config/bdd.properties";
 
     private Connection connection = null;
 
     static {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         Properties prop = new Properties();
-        try {
-            prop.load(classLoader.getResourceAsStream(PATH_FILE));
-            driver = prop.getProperty(DRIVER);
-            base = prop.getProperty(BASE);
-            user = prop.getProperty(USER);
-            password = prop.getProperty(PASSWORD);
+        try (InputStream resourceStream = classLoader.getResourceAsStream("config/hikari.properties")) {
+            prop.load(resourceStream);
+            config = new HikariConfig(prop);
+            hikari = new HikariDataSource(config);
         } catch (IOException e) {
             LOGGER.error("can't read the file {} ", PATH_FILE);
             throw new ExceptionDAO("error to read the file " + PATH_FILE, e);
@@ -52,13 +47,13 @@ public enum ConnectionManager {
      * @return the connection of the database
      */
     public Connection getConnection() {
-
+       
         try {
             if (connection == null || connection.isClosed()) {
-                Class.forName(driver);
-                connection = DriverManager.getConnection(base, user, password);
+                // Class.forName(driver);
+                connection = hikari.getConnection();
             }
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             LOGGER.error("error connection with the database ");
             throw new ExceptionDAO("error connection", e);
         }
--- src/main/java/com/excilys/computerDatabase/service/CompanyServiceImpl.java
+++ src/main/java/com/excilys/computerDatabase/service/CompanyServiceImpl.java
@@ -3,7 +3,6 @@ package com.excilys.computerDatabase.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.excilys.computerDatabase.mapper.CompanyMapper;
 import com.excilys.computerDatabase.model.Company;
 import com.excilys.computerDatabase.persistence.CompanyDAO;
 
