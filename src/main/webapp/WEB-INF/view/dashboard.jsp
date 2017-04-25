9dc97db7debbc9d9dd076ee201d81420603b7f3e
geoffrey deremetz <geoffrey.deremetz1@etu.univ-lorraine.fr>
2017-04-25 16:01:56 +0200

    [TEST] selenium dashboard

--- src/main/webapp/WEB-INF/view/dashboard.jsp
+++ src/main/webapp/WEB-INF/view/dashboard.jsp
@@ -7,7 +7,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<title>Computer Database</title>
+<title>dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="utf-8">
 <!-- Bootstrap -->
---
+++ src/test/java/com/excilys/computerDatabase/controller/TestDashboard.java
@@ -0,0 +1,54 @@
+package com.excilys.computerDatabase.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class TestDashboard {
+
+    private WebDriver driver;
+
+    private String baseUrl;
+
+    @BeforeClass
+    public static void beforeClass() {
+        ChromeDriverManager.getInstance().setup();
+    }
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        baseUrl = "http://localhost:8080/computerDatabase";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void after() throws IOException {
+      driver.quit();
+    }
+
+    @Test
+    public void testUrl() {
+        driver.get(baseUrl + "/dashboard");
+        assertEquals(driver.getTitle(), "dashboard");
+    }
+    
+    public void testRedirectAddComputeur(){
+        WebElement addComputer = driver.findElement(By.id("addComputer"));
+        addComputer.click();
+        assertEquals(driver.getCurrentUrl(), this.baseUrl + "/addComputer");
+    }
+    
+}
